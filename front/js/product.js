978c5f31f1c279cf0c319299f053da6d440e9b1e
Pierre-Henri Merrer <phmerrer@gmail.com>
2022-12-21 18:21:57 +0100

    E6 : Page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,79 @@
 /**
- * Représentation du format d'un produit
+ * Gère l'affichage et les intéractions de la page produit : product.html
  */
 
-class Product{
-    constructor(jsonProduct){
+// ----------------------------------------
+// Chargement du fichier config.json 
+//------------------------------------------
+async function loadConfig() {
+    let result = await fetch("../config.json");
+    return result.json();
+}
+
+
+// ----------------------------------------
+// Crée et initialise l'objet Product
+//------------------------------------------
+class Product {
+    constructor(jsonProduct) {
         jsonProduct && Object.assign(this, jsonProduct);
+        //console.log(jsonProduct);
     }
 }
+
+// ----------------------------------------
+// Récupere la valeur de 'id' dans l'URL
+//------------------------------------------
+ const productId = new URLSearchParams(window.location.search).get("id");
+ //console.log(productId);
+
+
+ if (productId !== null) {
+    loadConfig()
+    .then(data => {
+        config = data;
+        fetch(config.host + "/api/products/" + productId)
+        .then(data => data.json())
+        .then(jsonProduct => {
+            //console.log(jsonProduct);
+            let product = new Product(jsonProduct);
+            document.title = product.name;
+            try {
+                document.querySelector(".item__img").innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '.item__img' dans le fichier index.html", {Error});
+            }
+            try {
+                document.querySelector("#title").innerHTML += `${product.name}`
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '#title' dans le fichier index.html", {Error});
+            }
+            try {
+                document.querySelector("#price").innerHTML += `${product.price}`
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '#price' dans le fichier index.html", {Error});
+            }
+            try {
+                document.querySelector("#description").innerHTML += `${product.description}`
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '#description' dans le fichier index.html", {Error});
+            }
+            try {
+                product.colors.forEach(element => {
+                    document.querySelector("#colors").innerHTML += `<option value="${element}">${element}</option>`
+                    }); 
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '#colors' dans le fichier index.html", {Error});
+            }
+        })
+        .catch(Error => {
+            console.log("Problème d'accès à l'API - voir le fichier config.json", {Error});
+            alert("❌ Une erreur s'est produite. Le produit sélectionné n'a pas été trouvé !\n\n💡 Essayez d'actualisez la page\n\n🙏 Nous vous prions de nous excuser pour la gêne occasionnée !");
+        })
+    })
+ }
+ else{
+    console.log("Il manque l'ID du produit dans l'url pour afficher la page.");
+    alert("❌ Une erreur s'est produite.\n\n💡 Vous allez être redirigé vers notre catalogue de produits\n\n🙏 Nous vous prions de nous excuser pour la gêne occasionnée !");
+    window.location.href = "index.html";
+ }
--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,11 @@
 /**
- * Gère l'affichage et les intéractions de la page d'accueil
+ * Gère l'affichage et les intéractions de la page d'accueil : index.html
  */
 
 // ----------------------------------------
 // Chargement du fichier config.json 
 //------------------------------------------
-async function loadConfig(){
+async function loadConfig() {
     let result = await fetch("../config.json");
     return result.json();
 }
@@ -17,26 +17,39 @@ async function loadConfig(){
 class Product {
     constructor(jsonProduct) {
         jsonProduct && Object.assign(this, jsonProduct);
+        //console.log(jsonProduct);
     }
 }
 
 // ----------------------------------------
 // Insére les produits de l'API (fetch) dans la page d’accueil
 //------------------------------------------
-loadConfig().then(data => {
+
+loadConfig()
+.then(data => {
     config = data;
     fetch(config.host + "/api/products")
-        .then(date => date.json())
-        .then(jsonListProduct => {
-            for(let jsonProduct of jsonListProduct) {
-                let product = new Product(jsonProduct);
+    .then(data => data.json())
+    .then(jsonListProduct => {
+        //console.log(jsonListProduct);
+        for(let jsonProduct of jsonListProduct) {
+            let product = new Product(jsonProduct);
+            try {
                 document.querySelector("#items").innerHTML += `<a href="./product.html?id=${product._id}">
-                                                                    <article>
-                                                                    <img src="${product.imageUrl}" alt="${product.altTxt}">
-                                                                    <h3 class="productName">${product.name}</h3>
-                                                                    <p class="productDescription">${product.description}</p>
-                                                                    </article>
+                                                                <article>
+                                                                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                                                                <h3 class="productName">${product.name}</h3>
+                                                                <p class="productDescription">${product.description}</p>
+                                                                </article>
                                                                 </a>`
+            } catch (Error) {
+                console.log("Possible changement du sélecteur '#items' dans le fichier index.html", {Error});
             }
-        });
+            
+        }
+    })
+    .catch(Error => {
+        console.log("Problème d'accès à l'API - voir le fichier config.json", {Error});
+        alert("❌ Une erreur s'est produite et vous n'avez pas accès à notre catalogue de produits.\n\n💡 Essayez d'actualisez la page pour recharger notre catalogue\n\n🙏 Nous vous prions de nous excuser pour la gêne occasionnée !");
+    })
 })
